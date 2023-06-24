51dbc9a24b24d87add2a5a80c5554f71f925211f
Sharuk <msharuk.b10@gmail.com>
2023-06-24 12:32:21 +0530

    small changes

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -44,7 +44,7 @@ const storage = multer.diskStorage({
   
   const upload = multer({
     storage: storage,
-    limits: { fileSize: 10 * 1024 * 1024 }
+   
   });
 
   const auth = require('../middlewares/adminAuth');   
